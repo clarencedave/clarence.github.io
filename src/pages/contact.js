b748a3296ff183a5e93725f4a1181d81d1342f3f
Cla2 <clarencedave990@gmail.com>
2018-12-06 13:43:08 +0800

    init commit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,13 +3,31 @@ import React from 'react'
 
 import Layout from '../components/layout'
 
-const About = () => (
+const contact = () => (
   <Layout>
-    <h1>What is Seattle Angel?</h1>
-    <p>Seattle Angel is a not for profit organization, focused on educating angel investors, entrepreneurs, and prospective team members about the ins and outs of raising startup capital, scaling business operations, and related legal and financial issues. We welcome first-time investors and entrepreneurs. We provide opportunities to learn, including collaboration with seasoned male and female investors and investment workshops. We foster a collaborative culture that is a balance of professionalism and relaxed atmosphere. Seattle Angel brings together new and experienced investors living in greater Seattle and leverages their combined knowledge, resources, and experiences to find and fund early stage startups in the Pacific Northwest.</p>
+    <form name="contact" method="POST" netlify>
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
      </Layout>
 
      
 )
 
-export default About
+export default contact
