4c411476b8182b7161579738112a4764f8d1d9f1
Cla2 <clarencedave990@gmail.com>
2018-12-07 10:58:18 +0800

    commit

--- src/pages/form.js
+++ src/pages/form.js
@@ -6,11 +6,12 @@ const formPage = () => (
     <Layout>
 
      <form name = "contact" method = "post" netlify>
+     <input type="hidden" name="form-name" value="contact" />  
     <input Fname = "Fname " placeholder = "Your First Name" type ="text"/>
     <input Lname = "Lname " placeholder = "Your Last name" type ="text"/>
     <input address = "address " placeholder = "Address" type ="text"/>
     <input email = "mail " placeholder = "Email" type ="text"/>
-    <button>Send</button>
+    <button type = "submit">Send</button>
     </form>
 
 
