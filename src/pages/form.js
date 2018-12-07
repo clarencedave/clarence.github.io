94fe2ef3a8e85fc70eda3a9a5e1552957ff8a354
Cla2 <clarencedave990@gmail.com>
2018-12-07 13:52:30 +0800

    textarea

--- src/pages/form.js
+++ src/pages/form.js
@@ -6,7 +6,7 @@ const formPage = () => (
     <Layout>
 
      <form name = "contact" method = "post" netlify = "true">
-      
+     <input type="hidden" name="form-name" value="contact" />  
       <p>
       <label>First Name:   <input Fname = "Fname " placeholder = "Your First Name" type ="text"/></label>
       </p>
@@ -22,8 +22,15 @@ const formPage = () => (
       <p>
       <label>Email:   <input email = "mail " placeholder = "Email" type ="text"/></label>
       </p>
+
+      <label for="subject">Subject</label>
+      <textarea id="subject" name="subject" placeholder="Write something.." ></textarea>
+    
+
+
    <button type = "submit">Send</button>
    
+  
     </form>
 
 
