186e58fdeec4184c50ef3422097059aaa134feed
Cla2 <clarencedave990@gmail.com>
2018-12-17 11:36:11 +0800

    navigation icon added

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import logo from '../images/seattle-angel-logo.png'
-
+import blog from '../images/blog.png'
+import leader from '../images/leadership.png'
+import event from '../images/event.png'
+import entrepreneurs from '../images/entrepreneurs.png'
+import conference from '../images/conference.png'
+import sponsor from '../images/sponsor.png'
+import about from '../images/about.png'
+import mail from '../images/mail.png'
+import src from '../images/source.png'
 const Header = ({ siteTitle }) => (
   
   <div
@@ -29,15 +37,16 @@ const Header = ({ siteTitle }) => (
    <a   href="home" >
    <img src= {logo} alt="Angel"/>
       </a>
-       <a href="/"><li> Blog </li></a> 
-       <a href="about"><li>About</li> </a> 
-       <a href="resource"> <li>Resources</li> </a> 
-       <a href="contacts"><li>Leadership</li></a>
-       <a href="form"><li>Contact Us</li></a>
-       <a href="portfolio"><li>Events</li></a>
-       <a href="entrepreneurs"><li>Entrepreneurs</li></a>
-       <a href="sponsor"><li>Sponsors</li></a>
-       <a href="conference"><li>Conference</li></a>
+       <a href="/"><li> <img src = {blog} alt = "Blog"/> Blog </li></a> 
+       <a href="about"><li><img src = {about} alt = "About"/> About</li> </a> 
+       <a href="resource"> <li> <img src = {src} alt = "Resources"/> Resources</li> </a> 
+       <a href="contacts"><li><img src = {leader} alt = "Leadership"/> Leadership</li></a>
+       <a href="form"><li><img src = {mail} alt = "Email"/> Contact Us</li></a>
+       <a href="portfolio"><li><img src = {event} alt = "Event"/> Events</li></a>
+       <a href="entrepreneurs"><li><img src = {entrepreneurs} alt = "Entrepreneurs"/> Entrepreneurs</li></a>
+       <a href="sponsor"><li><img src = {sponsor} alt = "Sponsors"/> Sponsors</li></a>
+       <a href="conference"><li><img src = {conference} alt = "Conference"/> Conference</li></a>
+       
    </ul>
    </div>
    </nav>
